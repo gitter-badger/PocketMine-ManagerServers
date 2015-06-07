f3ee62c9daa01e80ae663b0c4c01a44be4277ba4
matcracker <matcracker98@gmail.com>
2015-06-07 22:38:46 +0200

    Forgot these... >.<

---
+++ Subs/Languages/Lang/Afrikaans.vb
@@ -0,0 +1,3 @@
+﻿Module Afrikaans
+
+End Module
--- Subs/Settings/Resetter.vb
+++ Subs/Settings/Resetter.vb
@@ -1,4 +1,18 @@
 ﻿Module Resetter
+    ' _____           _        _   __  __ _                   __  __                                   _____                              
+    '|  __ \         | |      | | |  \/  (_)                 |  \/  |                                 / ____|                             
+    '| |__) |__   ___| | _____| |_| \  / |_ _ __   ___ ______| \  / | __ _ _ __   __ _  __ _  ___ _ _| (___   ___ _ ____   _____ _ __ ___ 
+    '|  ___/ _ \ / __| |/ / _ \ __| |\/| | | '_ \ / _ \______| |\/| |/ _` | '_ \ / _` |/ _` |/ _ \ '__\___ \ / _ \ '__\ \ / / _ \ '__/ __|
+    '| |  | (_) | (__|   <  __/ |_| |  | | | | | |  __/      | |  | | (_| | | | | (_| | (_| |  __/ |  ____) |  __/ |   \ V /  __/ |  \__ \
+    '|_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|      |_|  |_|\__,_|_| |_|\__,_|\__, |\___|_| |_____/ \___|_|    \_/ \___|_|  |___/
+    '                                                                                   __/ |                                             
+    '                                                                                  |___/                                              
+    'Copyright (C) 2015  matcracker
+
+    'This program is free software: you can redistribute it and/or modify 
+    'it under the terms of the GNU Lesser General Public License as published by 
+    'the Free Software Foundation, either version 3 of the License, or 
+    '(at your option) any later version.
     Sub Resetter(ByRef nservers As Integer, ByRef checkpath As Object(), ByRef path As String(), ByRef checknservers As Object, ByRef checkfolderinstallation As Object, ByRef dirpath As Object, ByRef dirdata As Object, ByRef dirservername As Object, ByRef dirperformance As Object, ByRef dirinstallations As Object, ByRef dirlanguages As Object, ByRef direrrors As Object)
 
         Dim choosereset, reset As String
